d07b5c294c7806f7857cdea6b85097a832ddeed6
yongggnd <yongkerkwith@gmail.com>
2023-06-06 22:28:56 +0700

    imaeviewver for image message

--- lib/widgets/chat_page/message_bubble.dart
+++ lib/widgets/chat_page/message_bubble.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:dash_chat_2/dash_chat_2.dart';
+import 'package:easy_image_viewer/easy_image_viewer.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:sendbird_demo_app/services/sendbird_service.dart';
@@ -49,16 +50,21 @@ class MessageBubble extends StatelessWidget {
                 ? Uri.parse(media!.url).isAbsolute
                     ? ClipRRect(
                         borderRadius: BorderRadius.circular(15),
-                        child: CachedNetworkImage(
-                          imageUrl: media!.url,
-                          fit: BoxFit.cover,
-                          placeholder: (context, url) => const SizedBox(
-                              width: 20,
-                              height: 20,
-                              child: CircularProgressIndicator()),
-                          errorWidget: (context, url, error) {
-                            return Text("Cannot load $url");
-                          },
+                        child: InkWell(
+                          onTap: () => showImageViewer(
+                              context, Image.network(media!.url).image,
+                              swipeDismissible: true, useSafeArea: true),
+                          child: CachedNetworkImage(
+                            imageUrl: media!.url,
+                            fit: BoxFit.cover,
+                            placeholder: (context, url) => const SizedBox(
+                                width: 20,
+                                height: 20,
+                                child: CircularProgressIndicator()),
+                            errorWidget: (context, url, error) {
+                              return Text("Cannot load $url");
+                            },
+                          ),
                         ),
                       )
                     : const Text("invalid url")
